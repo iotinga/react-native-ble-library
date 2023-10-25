7af0c857764216d15de273c164653d2074f8207e
alerighi <alessandro.righi@iotinga.it>
2023-10-25 18:54:49 +0200

    fix error on rejecting promises

--- library/src/PromiseSequencer.ts
+++ library/src/PromiseSequencer.ts
@@ -2,6 +2,6 @@ export class PromiseSequencer {
   private pendingPromise: Promise<any> = Promise.resolve()
 
   execute<T>(fn: () => Promise<T>): Promise<T> {
-    return (this.pendingPromise = this.pendingPromise.then(() => fn()))
+    return (this.pendingPromise = this.pendingPromise.catch(() => {}).then(() => fn()))
   }
 }
