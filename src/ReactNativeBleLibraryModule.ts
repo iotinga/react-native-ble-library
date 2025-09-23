eee7083230e26e8188236666223b41c74b111686
alerighi <alessandro.righi@iotinga.it>
2025-09-23 19:02:32 +0200

    fix js code

--- src/ReactNativeBleLibraryModule.ts
+++ src/ReactNativeBleLibraryModule.ts
@@ -1,72 +1,42 @@
-import { NativeModule, requireNativeModule } from "expo";
-import { BleErrorCode } from "./BleError";
-import {
-  BleDeviceInfo,
-  ConnectionState,
-  BleServiceInfo,
-  BleServicesInfo,
-} from "./types";
+import { NativeModule, requireNativeModule } from 'expo'
+import { BleErrorCode } from './BleError'
+import { BleDeviceInfo, ConnectionState, BleServiceInfo, BleServicesInfo, ConnectOptions } from './types'
 
 export type ReactNativeBleLibraryModuleEvents = {
-  onError(data: { error: BleErrorCode; message: string }): void;
-  onScanResult(data: { devices: BleDeviceInfo[] }): void;
-  onCharValueChanged(data: {
-    service: string;
-    characteristic: string;
-    value: string;
-  }): void;
+  onError(data: { error: BleErrorCode; message: string }): void
+  onScanResult(data: { devices: BleDeviceInfo[] }): void
+  onCharValueChanged(data: { service: string; characteristic: string; value: string }): void
   onProgress(data: {
-    service: string;
-    characteristic: string;
-    current: number;
-    total: number;
-    transactionId: string;
-  }): void;
+    service: string
+    characteristic: string
+    current: number
+    total: number
+    transactionId: string
+  }): void
   onConnectionStateChanged(data: {
-    state: ConnectionState;
-    error: BleErrorCode | null;
-    message: string;
-    android?: { status: number };
-    ios?: { code: number; description: string };
-    services?: BleServiceInfo[];
-  }): void;
-};
+    state: ConnectionState
+    error: BleErrorCode | null
+    message: string
+    android?: { status: number }
+    ios?: { code: number; description: string }
+    services?: BleServiceInfo[]
+  }): void
+}
 
 declare class ReactNativeBleLibraryModule extends NativeModule<ReactNativeBleLibraryModuleEvents> {
-  initModule(): Promise<void>;
-  disposeModule(): Promise<void>;
-  scanStart(serviceUuids?: string[]): Promise<void>;
-  scanStop(): Promise<void>;
-  connect(id: string, mtu: number): Promise<BleServicesInfo>;
-  disconnect(): Promise<void>;
-  write(
-    transactionId: string,
-    service: string,
-    characteristic: string,
-    value: string,
-    chunkSize: number
-  ): Promise<void>;
-  read(
-    transactionId: string,
-    service: string,
-    characteristic: string,
-    size: number
-  ): Promise<string>;
-  subscribe(
-    transactionId: string,
-    service: string,
-    characteristic: string
-  ): Promise<void>;
-  unsubscribe(
-    transactionId: string,
-    service: string,
-    characteristic: string
-  ): Promise<void>;
-  readRSSI(transactionId: string): Promise<number>;
-  cancel(transactionId: string): Promise<void>;
+  initModule(): Promise<void>
+  disposeModule(): Promise<void>
+  scanStart(serviceUuids?: string[]): Promise<void>
+  scanStop(): Promise<void>
+  connect(id: string, mtu: number, options: ConnectOptions): Promise<BleServicesInfo>
+  disconnect(): Promise<void>
+  write(transactionId: string, service: string, characteristic: string, value: string, chunkSize: number): Promise<void>
+  read(transactionId: string, service: string, characteristic: string, size: number): Promise<string>
+  subscribe(transactionId: string, service: string, characteristic: string): Promise<void>
+  unsubscribe(transactionId: string, service: string, characteristic: string): Promise<void>
+  readRSSI(transactionId: string): Promise<number>
+  cancel(transactionId: string): Promise<void>
 }
 
 // This call loads the native module object from the JSI.
-export default requireNativeModule<ReactNativeBleLibraryModule>(
-  "ReactNativeBleLibrary"
-);
+export default requireNativeModule<ReactNativeBleLibraryModule>('ReactNativeBleLibrary')
